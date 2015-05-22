1c248ee5d88ef9778ef3be9ff440c469a4c3d4be
Brian Hartin <brian.hartin@banno.com>
2015-05-22 14:27:20 -0500

    Added verification of YAML to reconcile mode integ. test

--- src/test/groovy/co/freeside/betamax/ReconcileModeSpec.groovy
+++ src/test/groovy/co/freeside/betamax/ReconcileModeSpec.groovy
@@ -4,29 +4,29 @@ import co.freeside.betamax.proxy.jetty.SimpleServer
 import co.freeside.betamax.util.httpbuilder.BetamaxRESTClient
 import co.freeside.betamax.util.server.EchoHandler
 import co.freeside.betamax.util.server.HelloHandler
+import co.freeside.betamax.tape.NoSuchTapedRequestException
+import co.freeside.betamax.tape.yaml.YamlTapeLoader
+import co.freeside.betamax.tape.yaml.YamlTape
+import co.freeside.betamax.tape.Tape
+import co.freeside.betamax.tape.RecordedInteraction
 import groovyx.net.http.*
 import org.junit.Rule
 import spock.lang.*
 import static co.freeside.betamax.TapeMode.*
 import static co.freeside.betamax.util.FileUtils.newTempDir
 import static java.net.HttpURLConnection.HTTP_OK
+import static java.net.HttpURLConnection.HTTP_INTERNAL_ERROR
 import static org.apache.http.HttpHeaders.VIA
 import java.io.File
 
 @Stepwise
 class ReconcileModeSpec extends Specification {
-
         @Shared @AutoCleanup('deleteDir') File tapeRoot = newTempDir('tapes')
         @Rule Recorder recorder = new Recorder(tapeRoot: tapeRoot)
 
 	@AutoCleanup('stop') SimpleServer endpoint = new SimpleServer()
 
 	@Shared RESTClient http = new BetamaxRESTClient()
-
-	void setupSpec() {
-		if(reconciliationErrorFile().exists())
-                  reconciliationErrorFile().delete() == true
-	}
 
 	@Betamax(tape = 'reconcilemode', mode = WRITE_ONLY)
 	void 'proxy makes a real HTTP request the first time it gets a request for a URI'() {
@@ -53,31 +53,51 @@ class ReconcileModeSpec extends Specification {
 		then:
                 notThrown(HttpResponseException)
                 response.status == HTTP_OK
-                reconciliationErrorFile().exists() == false
+                tapeFile(recorder.reconciliationTape).exists() == false
         }
 
-        @Ignore('Not implemented yet')
         @Betamax(tape = 'reconcilemode', mode = RECONCILE)
         void 'Reconcile mode records reconciliation error tape when live response doesn\'t match taped response for the matching request'() {
                given:
-               endpoint.start(HelloHandler) // Gives different response for same uri
+               endpoint.start(EchoHandler) // Gives different response for same uri
 
                when:
-               HttpResponseDecorator response = http.get(uri: endpoint.url)
+               http.get(uri: endpoint.url)
 
                then:
-               response.status == HTTP_OK
-               reconciliationErrorFile().exists() == true
-               // Maybe verify some of the yaml
+               def e = thrown(HttpResponseException)
+               e.statusCode == HTTP_INTERNAL_ERROR
+
+               def recErrTape = recordedReconciliationTape()
+               recErrTape.size() == 1
+
+               // The unmatched response body was from the echo handler
+               recErrTape.interactions[0].response.bodyAsText.text =~ /User-Agent/
         }
 
-        @Ignore('Not implemented yet')
-        void 'Reconcile mode behaves identically to read-only mode if no matching request found'()   {
-          // Spin up svr, make request to /otherpath, which svr responds to, but no
-          // recorded request is on tape.  Should get an error.
+        @Betamax(tape = 'reconcilemode', mode = RECONCILE)
+        void 'Reconcile mode produces error if no matching request found'()   {
+          given:
+          endpoint.start(HelloHandler)
+
+          when:
+          http.get(uri: endpoint.url + "?q=foo")
+
+          then:
+          def e = thrown(HttpResponseException)
+          e.statusCode == HTTP_INTERNAL_ERROR
         }
 
-        def reconciliationErrorFile() {
-          new File([tapeRoot, 'reconcilemode.reconciliation-errors.yaml'].join(File.separator))
+        def recordedReconciliationTape() {
+          Tape reconciliationTape = recorder.reconciliationTape
+          recorder.ejectTape()
+
+          File file = tapeFile(reconciliationTape)
+          file.exists() == true
+          YamlTape loadedTape = file.withReader(YamlTapeLoader.FILE_CHARSET) {YamlTape.readFrom(it)}
+        }
+
+        File tapeFile(Tape tape) {
+          new YamlTapeLoader(recorder.tapeRoot).fileFor(tape.name)
         }
 }
